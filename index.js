e81ae452819ee1d8bd6562b545bb7fefa200d615
Ian Storz <istorz@nestlabs.com>
2016-06-19 17:46:32 -0600

    countInversions implemented

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 'use strict';
 
-var countInversions = function(array) {
-  return 0;
+/*
+ * sigils
+ *   n: number
+ *   s: sorted
+ */
+
+const count = function(A) {
+  let sortedArray = A;
+  let inversions = 0;
+
+  const n = A.length;
+
+  if (n <= 1) return { sortedArray, inversions };
+
+  const B = A.slice(0, n/2);
+  const C = A.slice(n/2);
+
+  const X = count(B);
+  const Y = count(C);
+  const Z = merge(X.sortedArray, Y.sortedArray);
+
+  sortedArray = Z.sortedArray;
+  inversions = X.inversions + Y.inversions + Z.inversions;
+
+  return { sortedArray, inversions };
 };
 
-module.exports = countInversions;
+const merge = function(B, C, sD, count) {
+  const nB = B.length;
+  const nC = C.length;
+  const D = sD || [];
+  let c = count || 0;
+
+  switch (true) {
+    case nB === 0 && nC === 0:
+      return { sortedArray: D, inversions: count };
+    case nB === 0 && nC !== 0:
+      D.push(C[0]);
+      return merge([], C.slice(1), D, c);
+    case nB !== 0 && nC === 0:
+      D.push(B[0]);
+      return merge(B.slice(1), [], D, c);
+    case B[0] <= C[0]:
+      D.push(B[0]);
+      return merge(B.slice(1), C, D, c);
+    case B[0] > C[0]:
+      D.push(C[0]);
+      c = c + nB;
+      return merge(B, C.slice(1), D, c);
+  }
+};
+
+module.exports = count;
--- tests/unit/count-inversions-test.js
+++ tests/unit/count-inversions-test.js
@@ -5,11 +5,30 @@ var count = require('../../index');
 
 describe('countInversions', function() {
   it('should count the correct number of inversions in an array', function() {
-    assert.equal( 0, count([]));
-    assert.equal( 0, count([1]));
-    assert.equal( 0, count([1, 2]));
-    assert.equal( 0, count([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-    assert.equal( 0, count([1, 2, 2, 3, 3, 3, 4, 4, 4]));
-    assert.equal( 0, count([1, 1, 1, 1, 1, 1, 1, 1, 1]));
+    assert.equal(0, count([]).inversions);
+    assert.equal(0, count([1]).inversions);
+    assert.equal(0, count([1, 2]).inversions);
+    assert.equal(0, count([1, 2, 2, 3, 3, 3]).inversions);
+    assert.equal(0, count([1, 1, 1, 1, 1, 1]).inversions);
+
+    assert.equal(0,  count([1, 2, 3, 4, 5, 6]).inversions);
+    assert.equal(1,  count([1, 2, 3, 4, 6, 5]).inversions);
+    assert.equal(2,  count([1, 2, 3, 6, 4, 5]).inversions);
+    assert.equal(3,  count([1, 2, 6, 3, 4, 5]).inversions);
+    assert.equal(4,  count([1, 6, 2, 3, 4, 5]).inversions);
+    assert.equal(5,  count([6, 1, 2, 3, 4, 5]).inversions);
+    assert.equal(6,  count([6, 1, 2, 3, 5, 4]).inversions);
+    assert.equal(7,  count([6, 1, 2, 5, 3, 4]).inversions);
+    assert.equal(8,  count([6, 1, 5, 2, 3, 4]).inversions);
+    assert.equal(9,  count([6, 5, 1, 2, 3, 4]).inversions);
+    assert.equal(10, count([6, 5, 1, 2, 4, 3]).inversions);
+    assert.equal(11, count([6, 5, 1, 4, 2, 3]).inversions);
+    assert.equal(12, count([6, 5, 4, 1, 2, 3]).inversions);
+    assert.equal(13, count([6, 5, 4, 1, 3, 2]).inversions);
+    assert.equal(14, count([6, 5, 4, 3, 1, 2]).inversions);
+    assert.equal(15, count([6, 5, 4, 3, 2, 1]).inversions);
+
+    assert.equal(0, count([289, 2378, 98950]).inversions);
+    assert.equal(6, count([4676, 50, 49, 49, 50, 58989]).inversions);
   });
 });
